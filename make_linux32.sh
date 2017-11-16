7c7a23250fe7ba8acc057ffad84fc61911bfba34
DESKTOP-RDLLC6S\Administrator <703016035@qq.com>
2017-11-16 11:51:46 +0800

    linux编译修改

--- make_linux32.sh
+++ make_linux32.sh
@@ -2,4 +2,5 @@ mkdir -p build_linux32 && cd build_linux32
 cmake -DCMAKE_C_FLAGS=-m32 -DCMAKE_CXX_FLAGS=-m32 -DCMAKE_SHARED_LINKER_FLAGS=-m32 ../
 cd ..
 cmake --build build_linux32 --config Release
+cp build_linux32/kcp.so Plugins/x86/kcp.so
 rm -rf build_linux32
--- make_linux64.sh
+++ make_linux64.sh
@@ -2,4 +2,5 @@ mkdir -p build_linux64 && cd build_linux64
 cmake ../
 cd ..
 cmake --build build_linux64 --config Release
+cp build_linux64/kcp.so Plugins/x86_64/kcp.so
 rm -rf build_linux64
